ea02eec8a87acb71ad1ba22ca5d25e2fa48a3804
michaeljuerobin <76797163+michaeljuerobin@users.noreply.github.com>
2021-01-03 01:13:53 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,16 +2,12 @@
 -- 0866
 -- December 30, 2020
 
---[[
-    
-]]
-
-
-local Players = game:GetService("Players")
 
 
 local player do
     -- Player handler:
+    local Players = game:GetService("Players")
+
     repeat 
         player = Players.LocalPlayer
     until player
@@ -24,82 +20,87 @@ local player do
 end
 
 
-local Define do
+local Define, DefineCClosure do
     local GENV = getgenv()
 
     function Define(...)
         local aliases = table.pack(...)
         local value = table.remove(aliases, aliases.n)
-    
-        value = (type(value) == "function" and newcclosure(value) or value)
-    
         for _,key in ipairs(aliases) do
             GENV[key] = value
         end
+        return value
+    end
+
+    function DefineCClosure(...)
+        local aliases = table.pack(...)
+        local value = newcclosure(table.remove(aliases, aliases.n))
+        for _,key in ipairs(aliases) do
+            GENV[key] = value
+        end
+        return value
     end
 end
 
 
-local AddMethod, GetMethod do
+local AddMember, GetMember do
     local metatable = getrawmetatable(game)
     local __index = metatable.__index
     local __namecall = metatable.__namecall
 
-    local instanceMethods = {}
+    local instanceMembers = {}
 
     setreadonly(metatable, false)
 
     metatable.__namecall = newcclosure(function(self, ...)
-        if (
-            checkcaller()
-            and instanceMethods[self]
-            and instanceMethods[self][getnamecallmethod()]
-        ) then
-            return instanceMethods[self][getnamecallmethod()](self, ...)
+        if (checkcaller() and GetMember(self, getnamecallmethod())) then
+            return instanceMembers[self][getnamecallmethod()](self, ...)
         else
             return __namecall(self, ...)
         end
     end)
 
-    metatable.__index = newcclosure(function(self, ...)
-        if (
-            checkcaller()
-            and instanceMethods[self]
-            and instanceMethods[self][getnamecallmethod()]
-        ) then
-            return instanceMethods[self][getnamecallmethod()]
+    metatable.__index = newcclosure(function(self, index)
+        if (checkcaller() and GetMember(self, index)) then
+            return instanceMembers[self][index]
         else
-            return __index(self, ...)
+            return __index(self, index)
         end
     end)
 
-    function AddMethod(instance, ...)
-        local aliases = table.pack(...)
-        local func = table.remove(aliases, aliases.n)
+    function AddMember(instance, aliases, func)
+        local memberList = instanceMembers[instance]
 
-        local methodList = (instanceMethods[instance] or {})
+        if (not memberList) then
+            memberList = {}
+            instanceMembers[instance] = memberList
+        end
 
-        for _,methodName in ipairs(aliases) do
-            methodList[methodName] = func
+        if (type(func) == "function" and islclosure(func)) then
+            func = newcclosure(func)
+        end
+
+        for _,memberName in ipairs(aliases) do
+            memberList[memberName] = func
         end
     end
 
-    function GetMethod(instance, methodName)
-        return (instanceMethods[instance] and instanceMethods[instance][methodName])
+    function GetMember(instance, memberName)
+        return (instanceMembers[instance] and instanceMembers[instance][memberName])
     end
+
+    setreadonly(metatable, true)
 
 end
 
 
 do
     -- File system:
-    Define("loadfile", function(file)
-        assert(isfile(file), "Argument #1 'file' must be a file")
+    DefineCClosure("loadfile", function(file)
         return loadstring(readfile(file), file)
     end)
 
-    Define("dofile", function(file)
-        assert(isfile(file), "Argument #1 'file' must be a file")
+    DefineCClosure("dofile", function(file)
         return loadfile(file)(file)
     end)
 
@@ -139,7 +140,7 @@ do
     local getloadedmodules = getloadedmodules
 
     -- Auto wrap hook in newcclosure:
-    Define("hookfunction", "hookfunc", function(func, hook)
+    DefineCClosure("hookfunction", "hookfunc", function(func, hook)
         if (iscclosure(func) and islclosure(hook)) then
             return hookfunction(func, newcclosure(hook))
         else
@@ -148,10 +149,7 @@ do
     end)
 
     -- Unlock modules before requiring:
-    Define("require", function(moduleScript)
-        if (not checkcaller()) then
-            return require(moduleScript)
-        end
+    DefineCClosure("require", function(moduleScript)
         unlockModule(moduleScript)
         local module = require(moduleScript)
         lockModule(moduleScript)
@@ -172,52 +170,117 @@ end
 
 
 do
-    -- Methods:
+    -- Members:
     local httpGetAsync = game.HttpGetAsync
     local httpPostAsync = game.HttpPostAsync
 
-    AddMethod(game, "HttpGetAsync", "HttpGet", function(self, url)
+    AddMember(game, { "HttpGetAsync", "HttpGet" }, function(self, url)
         local networkMode = getnetworkmode()
         setnetworkmode(3)
-        httpGetAsync(self, url)
+        local result = httpGetAsync(self, url)
         setnetworkmode(networkMode)
+        return result
     end)
 
-    AddMethod(game, "HttpPostAsync", "HttpPost", function(self, url)
+    AddMember(game, { "HttpPostAsync", "HttpPost" }, function(self, ...)
         local networkMode = getnetworkmode()
         setnetworkmode(3)
-        httpPostAsync(self, url)
+        local result = httpPostAsync(self, ...)
         setnetworkmode(networkMode)
+        return result
     end)
 
-    AddMethod(game, "GetObjects", function(self, assetId)
+    AddMember(game, { "GetObjects" }, function(self, assetId)
+        if (type(assetId) == "number") then
+            assetId = "rbxassetid://" .. assetId
+        end
         return { game:GetService("InsertService"):LoadLocalAsset(assetId) }
     end)
 
-    AddMethod(
-        game:GetService("MarketplaceService"),
-        "PromptBundlePurchase",
-        "PromptPurchase",
-        "PromptSubscriptionCancellation",
-        "PromptProductPurchase",
-        "PromptGamePassPurchase",
-        "PromptPremiumPurchase",
-        "PromptSubscriptionPurchase",
-        function()
-        end
-    )
+    AddMember(game, { "Players" }, game:GetService("Players"))
+    AddMember(game, { "Lighting" }, game:GetService("Lighting"))
+    AddMember(game, { "ReplicatedStorage" }, game:GetService("ReplicatedStorage"))
+    AddMember(game, { "CoreGui" }, game:GetService("CoreGui"))
 
 end
 
 
 do
-    -- Aliases:
+    -- Member blacklist:
+    local MEMBER_BLACKLIST = {
+        [game] = {
+            "OpenVideosFolder",
+            "OpenScreenshotsFolder",
+        };
+
+        [game:GetService("BrowserService")] = {
+            "OpenBrowserWindow",
+        };
+
+        [game:GetService("MarketplaceService")] = {
+            "PromptBundlePurchase",
+            "PromptPurchase",
+            "PromptSubscriptionCancellation",
+            "PromptProductPurchase",
+            "PromptGamePassPurchase",
+            "PromptPremiumPurchase",
+            "PromptSubscriptionPurchase",
+        };
+    }
+
+    for instance, names in pairs(MEMBER_BLACKLIST) do
+        AddMember(instance, names, error)
+    end
+
+end
+
+
+do
+    -- Functions & aliases:
     Define("bit", bit32)
     Define("checkclosure", iskrnlclosure)
     Define("http_request", request)
 
+    Define("isluau", function()
+        return true
+    end)
+
     for key, value in pairs(debug) do
         Define(key, value)
+    end
+
+end
+
+
+do
+    -- Krnl global:
+    local Krnl = Define("Krnl", {
+        Base64 = {
+            Encode = base64_encode;
+            Decode = base64_decode;
+        };
+
+        Vendor = {
+            Maid = "https://raw.githubusercontent.com/Quenty/NevermoreEngine/version2/Modules/Shared/Events/Maid.lua";
+            Promise = "https://gist.github.com/richie0866/f7c56370664cd8b6d13b02e70529fc86/raw/6e945905b6a1106276cf8b128893c2b50997a00f/Promise.lua";
+            Signal = "https://gist.githubusercontent.com/richie0866/98879ede8725238d6eb8523774ec31b9/raw/7a4a57334056de0fe84f602315ba5c45524b57d9/Signal.lua";
+            Thread = "https://gist.githubusercontent.com/richie0866/89a30f80b1562678a2d554c18c0a022f/raw/b53d733b2a52788648008d3bd7e553ea286f1d1e/Thread.lua";
+            Hook = "https://gist.githubusercontent.com/richie0866/dfff74c366c141a681b580f613f7962f/raw/d89456887e62a8d5a36da0317f25454c433fa0bb/Hook.lua";
+        };
+    })
+
+    local modules = {}
+
+    function Krnl:Require(moduleName)
+        if (modules[moduleName]) then
+            return modules[moduleName]
+        elseif (self.Vendor[moduleName]) then
+            local module = loadstring(game:HttpGetAsync(self.Vendor[moduleName]))()
+            modules[moduleName] = module
+            return module
+        else
+            error("Attempt to require an unsupported module (Krnl:Require(\"" .. tostring(moduleName) .. "\"))")
+        end
     end
 
 end
